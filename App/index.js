cb03cb4d7f85c8280467e71fbf71948a82bb9bb1
admitkok <kokov.alexander@gmail.com>
2024-05-10 15:07:29 +0200

    physics

--- App/index.js
+++ App/index.js
@@ -12,13 +12,15 @@ import {
     Clock,
     SphereGeometry,
     IcosahedronGeometry,
-    Vector3,
+    Vector3, MathUtils,
 } from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import Stats from 'stats.js';
 
 import vertex from './shaders/index.vert';
 import fragment from './shaders/index.frag';
+import {World, Sphere, Body, Vec3 } from "cannon-es";
+import {random} from "gsap/gsap-core";
 
 export default class App {
     constructor() {
@@ -36,22 +38,33 @@ export default class App {
         // CAMERA
         const aspect = window.innerWidth / window.innerHeight;
         this._camera = new PerspectiveCamera(60, aspect, 0.1, 50);
-        this._camera.position.z = 5;
+        this._camera.position.z = 30;
 
         // SCENE
         this._scene = new Scene();
+
+        this._world = new World();
+        this._world.gravity.set(0, -1, 0);  // No gravity in any direction
+
 
         // CLOCK
         this._clock = new Clock();
 
         // INIT PLANE
-        //this._initPlane();
+        // this._initPlane();
 
         // INIT PLANE
-        //this._initAttribute();
+        // this._initAttribute();
 
         // ICOSAEDRON
-        this._initIcosahedron();
+        const elements = []
+        for (let i = 0; i < 15; i++) {
+            const el = this._initIcosahedron();
+            elements.push(el)
+        }
+
+        this._elements = elements
+
 
         // CONTROLS
         const controls = new OrbitControls(this._camera, this._gl.domElement);
@@ -76,8 +89,8 @@ export default class App {
     }
 
     _initAttribute() {
-        //const g = new PlaneGeometry(1, 1, 30, 30);
-        const g = new SphereGeometry(1, 300, 300);
+        // const g = new PlaneGeometry(1, 1, 30, 30);
+        const g = new SphereGeometry(0.5, 300, 300);
 
         const randomArray = [];
         const amount = g.attributes.position.count;
@@ -100,7 +113,6 @@ export default class App {
             const r = Math.random();
             const g = Math.random();
             const b = Math.random();
-
             colorArray.push(r, g, b);
         }
 
@@ -122,11 +134,18 @@ export default class App {
         this._mesh = mesh;
 
         this._scene.add(mesh);
+
+        const shape = new Sphere(1);
+        const body = new Body({
+            mass: 1,
+            position: new Vec3(0, 0, 0),
+            shape: shape
+        });
+        this._world.addBody(body);
     }
 
     _initIcosahedron() {
-        const g = new IcosahedronGeometry(1, 40);
-        //const g = new PlaneGeometry(3,3, 400, 400)
+        const g = new IcosahedronGeometry(0.5, 40);
         const m = new ShaderMaterial({
             vertexShader: vertex,
             fragmentShader: fragment,
@@ -134,13 +153,32 @@ export default class App {
                 uTime: { value: 0 },
                 uColorA: { value: new Color(0x00ffee) },
                 uColorB: { value: new Color(0xff52ff) },
-                uMouse: { value: new Vector3(0, 0, 1) },
+                uMouse: { value: new Vector3(0, 0, 0) },
             },
         });
 
         const mesh = new Mesh(g, m);
         this._ico = mesh;
         this._scene.add(mesh);
+
+
+        const shape = new Sphere(1);
+        const body = new Body({
+            mass: 10,
+            position: new Vec3(
+                MathUtils.randFloat(-30, 30),
+                MathUtils.randFloat(-30, 30),
+                MathUtils.randFloat(-30, 30),
+            ),
+            shape: shape
+        });
+        this._world.addBody(body);
+
+        // bind mesh body
+        mesh.userData.body = body
+        this._test = mesh
+
+        return mesh
     }
 
     _initEvents() {
@@ -151,6 +189,10 @@ export default class App {
     _onMouseMove(e) {
         const x = (e.clientX / window.innerWidth) * 2 - 1;
         this._ico.material.uniforms.uMouse.value.x = x;
+        const y = -(e.clientY / window.innerHeight) * 2 + 1;
+        this._ico.material.uniforms.uMouse.value.y = y;
+        this._world.gravity.x = x * 1;
+        this._world.gravity.y = y * 1;
     }
 
     _resize() {
@@ -161,9 +203,20 @@ export default class App {
         this._camera.updateProjectionMatrix();
     }
 
+
+
     _animate() {
         this._stats.begin();
         this._clock.delta = this._clock.getDelta();
+        this._world.step( this._clock.delta );
+
+        console.log(this._test.userData.body)
+        //this._test.position.copy(this._test.userData.body.position)
+        if(this._clock.elapsedTime > 2){
+            this._elements.forEach(el => {
+                el.position.copy(el.userData.body.position)
+            })
+        }
 
         // SPHERE
         /*this._mesh.material.uniforms.uIntensity.value = Math.tan(
@@ -175,6 +228,8 @@ export default class App {
 
         // ICO
         this._ico.material.uniforms.uTime.value = this._clock.elapsedTime;
+
+
 
         this._gl.render(this._scene, this._camera);
         this._stats.end();
